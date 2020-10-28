6425f572d459b6137531e6071e1b1a15591cadd7
Jake Kara <jake@jakekara.com>
2020-10-28 17:08:25 -0400

    Margo refactor

--- notebook_description_language/parser.py
+++ notebook_description_language/parser.py
@@ -1,81 +1,21 @@
-from lark import Lark, Transformer
+from lark import Tree, Lark
+from .margo_transformer import MargoTransformer
 import os
 
 
-def load_parser(file=os.path.join(os.path.split(__file__)[0], "nbd.lark")):
-    return Lark(open(file).read(), start="module")
+def grammar():
+    grammar_file_path = os.path.join(os.path.split(__file__)[0], "margo.lark")
+    grammar_content = open(grammar_file_path).read()
+    return Lark(grammar_content, start="block", regex=True)
 
 
-def parse(code_block):
-    parser = load_parser()
-    tree = parser.parse(code_block)
-    return NBDTransformer().transform(tree)
+def get_tree(source: str):
+    return grammar().parse(source)
 
 
-class NBDTransformer(Transformer):
+def transform(tree):
+    return MargoTransformer().transform(tree)
 
-    def empty_module(self, v):
-        return ["NO_CODE"]
 
-    def ignore_cell(self, v):
-        return "IGNORE_CELL"
-
-    def split_cell(self, s):
-        return "SPLIT_CELL"
-
-    def endblock(self, e):
-        return "END_BLOCK"
-
-    def expression(self, e):
-        (e, ) = e
-        return e
-
-    def data_type(self, d):
-        (d, ) = d
-        return d
-
-    def number(self, n):
-        (n, ) = n
-        try:
-            return int(n.value)
-        except BaseException:
-            pass
-        try:
-            return float(n.value)
-        except BaseException:
-            pass
-        raise Exception(f"Invalid number: {n.value}")
-
-    def string(self, s):
-        """
-            Remove quote chars from string
-        """
-        (s,) = s
-        return s[1:-1]
-        return s.value
-
-    def key(self, k):
-        """
-        Unwrap and get string. It won't have
-        quote chars because it conforms to
-        symbol regex: alphanums, . and _
-        """
-        (k, ) = k
-        return k.value
-
-    # A module is a list of expressions
-    # separated by endblocks
-    module = list
-
-    def named_list(self, k):
-        """
-            Unpack the values into an array
-            so it's always a two-element tuple
-        """
-        (k, *vals) = k
-        return (k, vals)
-
-    # directly convert to Python values
-    def null(self, _): return None
-    def true(self, _): return True
-    def false(self, _): return False
+def parse(source: str):
+    return transform(get_tree(source))
