72e12d57eff352f742e8eab456180d29d12aef60
Jake Kara <jake@jakekara.com>
2020-12-02 10:09:58 -0500

    Move generated HTML to /docs for GitHub

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # pdoc --html margo_parser -o docs --force
-sphinx-apidoc-3.8 -o docs/source/ margo_parser && \
-cd docs && \
+sphinx-apidoc-3.8 -o sphinx/source/ margo_parser && \
+cd sphinx && \
 make clean && \
 make html && \
+mv build/html ../docs && \
 cd ..
